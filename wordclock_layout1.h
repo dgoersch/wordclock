5c0a8c80e8930a18afaa7d960f7bb8388c35f6a5
dgoersch <dgoersch@users.noreply.github.com>
2016-06-20 11:07:23 +0200

    Create wordclock_layout1.h

---
+++ wordclock_layout1.h
@@ -0,0 +1,173 @@
+/* 
+(c) 2016 - Dominique Görsch - <mail@dgoersch.info>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+der GNU General Public License, wie von der Free Software Foundation,
+Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+Siehe die GNU General Public License für weitere Details.
+Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>. 
+*/
+
+/*
+ *   0 X--X--X--X--X--X--X--X--X--X--X 10
+ *                                   |
+ *  21 X--X--X--X--X--X--X--X--X--X--X 11
+ *     |
+ *  22 X--X--X--X--X--X--X--X--X--X--X 32
+ *                                   |
+ *  43 X--X--X--X--X--X--X--X--X--X--X 33
+ *     |
+ *  44 X--X--X--X--X--X--X--X--X--X--X 54 
+ *                                   |
+ *  65 X--X--X--X--X--X--X--X--X--X--X 55
+ *     |
+ *  66 X--X--X--X--X--X--X--X--X--X--X 76
+ *                                   |
+ *  87 X--X--X--X--X--X--X--X--X--X--X 77
+ *     |
+ *  88 X--X--X--X--X--X--X--X--X--X--X 98
+ *                                   |
+ * 109 X--X--X--X--X--X--X--X--X--X--X 99
+ *     |
+ *     `---------X--X--X--X 
+ *              110      113
+ */
+
+#define X1Y1     0
+#define X2Y1     1
+#define X3Y1     2
+#define X4Y1     3
+#define X5Y1     4
+#define X6Y1     5
+#define X7Y1     6
+#define X8Y1     7
+#define X9Y1     8
+#define X10Y1    9
+#define X11Y1   10
+
+#define X1Y2    21
+#define X2Y2    20
+#define X3Y2    19
+#define X4Y2    18
+#define X5Y2    17
+#define X6Y2    16
+#define X7Y2    15
+#define X8Y2    14
+#define X9Y2    13
+#define X10Y2   12
+#define X11Y2   11
+
+#define X1Y3    22
+#define X2Y3    23
+#define X3Y3    24
+#define X4Y3    25
+#define X5Y3    26
+#define X6Y3    27
+#define X7Y3    28
+#define X8Y3    29
+#define X9Y3    30
+#define X10Y3   31
+#define X11Y3   32
+
+#define X1Y4    43
+#define X2Y4    42
+#define X3Y4    41
+#define X4Y4    40
+#define X5Y4    39
+#define X6Y4    38
+#define X7Y4    37
+#define X8Y4    36
+#define X9Y4    35
+#define X10Y4   34
+#define X11Y4   33
+
+#define X1Y5    44
+#define X2Y5    45
+#define X3Y5    46
+#define X4Y5    47
+#define X5Y5    48
+#define X6Y5    49
+#define X7Y5    50
+#define X8Y5    51
+#define X9Y5    52
+#define X10Y5   53
+#define X11Y5   54
+
+#define X1Y6    65
+#define X2Y6    64
+#define X3Y6    63
+#define X4Y6    62
+#define X5Y6    61
+#define X6Y6    60
+#define X7Y6    59
+#define X8Y6    58
+#define X9Y6    57
+#define X10Y6   56
+#define X11Y6   55
+
+#define X1Y7    66
+#define X2Y7    67
+#define X3Y7    68
+#define X4Y7    69
+#define X5Y7    70
+#define X6Y7    71
+#define X7Y7    72
+#define X8Y7    73
+#define X9Y7    74
+#define X10Y7   75
+#define X11Y7   76
+
+#define X1Y8    87
+#define X2Y8    86
+#define X3Y8    85
+#define X4Y8    84
+#define X5Y8    83
+#define X6Y8    82
+#define X7Y8    81
+#define X8Y8    80
+#define X9Y8    79
+#define X10Y8   78
+#define X11Y8   77
+
+#define X1Y9    88
+#define X2Y9    89
+#define X3Y9    90
+#define X4Y9    91
+#define X5Y9    92
+#define X6Y9    93
+#define X7Y9    94
+#define X8Y9    95
+#define X9Y9    96
+#define X10Y9   97
+#define X11Y9   98
+
+#define X1Y10  109
+#define X2Y10  108
+#define X3Y10  107
+#define X4Y10  106
+#define X5Y10  105
+#define X6Y10  104
+#define X7Y10  103
+#define X8Y10  102
+#define X9Y10  101
+#define X10Y10 100
+#define X11Y10  99
+
+#define MIN1   110
+#define MIN2   111
+#define MIN3   112
+#define MIN4   113
