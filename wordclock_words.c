1c1606badba04bc8c31b0f6930906df29580dd4b
dgoersch <dgoersch@users.noreply.github.com>
2016-06-20 11:06:52 +0200

    Update wordclock_words.c

--- wordclock_words.c
+++ wordclock_words.c
@@ -22,190 +22,55 @@ Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
 Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>. 
 */
 
-void min_ONE() {
-  toStrip(MIN1);
-}
+/*
+ *     x x x x x x x x x x x
+ *     1 2 3 4 5 6 7 8 9 1 1
+ *                       0 0
+ *  
+ * y1  E S K I S T A F Ü N F
+ * y2  Z E H N Z W A N Z I G
+ * y3  D R E I V I E R T E L
+ * y4  V O R F U N K N A C H
+ * y5  H A L B A E L F Ü N F
+ * y6  K E I N S L M Z W E I
+ * y7  D R E I A U J V I E R
+ * y8  S E C H S N L A C H T
+ * y9  S I E B E N Z W Ö L F
+ * y10 Z E H N E U N K U H R
+ */
+ 
+#include "wordclock_layout1.h"
 
-void min_TWO() {
-  toStrip(MIN2);
-}
+const int min_ONE[]      {MIN1};
+const int min_TWO[]      {MIN2};
+const int min_THREE[]    {MIN3};
+const int min_FOUR[]     {MIN4};
 
-void min_THREE() {
-  toStrip(MIN3);
-}
+const int word_KEIN[]    {X1Y6, X2Y6, X3Y6, X4Y6};
+const int word_FUNK[]    {X4Y4, X5Y4, X5Y4, X7Y4};
 
-void min_FOUR() {
-  toStrip(MIN4);
-}
+const int word_ES[]      {X1Y1, X2Y1};
+const int word_IST[]     {X4Y1, X5Y1, X6Y1};
+const int word_UHR[]     {X9Y10, X10Y10, X11Y10};
 
-void word_KEIN() {
-  toStrip(X1Y6);
-  toStrip(X2Y6);
-  toStrip(X3Y6);
-  toStrip(X4Y6);
-}
+const int word_VOR[]     {X1Y4, X2Y4, X3Y4};
+const int word_NACH[]    {X8Y4, X9Y4, X10Y4, X11Y4};
+const int word_VIERTEL[] {X5Y3, X6Y3, X7Y3, X8Y3, X9Y3, X10Y3, X11Y3};
+const int word_HALB[]    {X1Y5, X2Y5, X3Y5, X4Y5};
 
-void word_FUNK() {
-  toStrip(X4Y4);
-  toStrip(X5Y4);
-  toStrip(X6Y4);
-  toStrip(X7Y4);
-}
-
-void word_ES() {
-  toStripe(X1Y1);
-  toStripe(X2Y1);
-}
-
-void word_IST() {
-  toStripe(X4Y1);
-  toStripe(X5Y1);
-  toStripe(X6Y1);
-}
-
-void word_UHR() {
-  toStrip(X9Y10);
-  toStrip(X10Y10);
-  toStrip(X11Y10);
-}
-
-void word_VOR() {
-  toStrip(X1Y4);
-  toStrip(X2Y4);
-  toStrip(X3Y4);
-}
-
-void word_NACH() {
-  toStrip(X8Y4);
-  toStrip(X9Y4);
-  toStrip(X10Y4);
-  toStrip(X11Y4);
-}
-
-void word_VIERTEL() {
-  toStrip(X5Y3);
-  toStrip(X6Y3);
-  toStrip(X7Y3);
-  toStrip(X8Y3);
-  toStrip(X9Y3);
-  toStrip(X10Y3);
-  toStrip(X11Y3);
-}
-
-void word_HALB() {
-  toStrip(X1Y5);
-  toStrip(X2Y5);
-  toStrip(X3Y5);
-  toStrip(X4Y5);
-}
-
-void word_EINS(bool s) {
-  toStrip(X2Y6);
-  toStrip(X3Y6);
-  toStrip(X4Y6);
-  if (s) { toStrip(X5Y6); }
-}
-
-void word_ZWEI() {
-  toStrip(X8Y6);
-  toStrip(X9Y6);
-  toStrip(X10Y6);
-  toStrip(X11Y6);
-}
-
-void word_DREI() {
-  toStrip(X1Y7);
-  toStrip(X2Y7);
-  toStrip(X3Y7);
-  toStrip(X4Y7);
-}
-
-void word_VIER() {
-  toStrip(X8Y7);
-  toStrip(X9Y7);
-  toStrip(X10Y7);
-  toStrip(X11Y7);
-}
-
-void word_FUENF1() {
-  toStripe(X8Y1);
-  toStripe(X9Y1);
-  toStripe(X10Y1);
-  toStripe(X11Y1);
-}
-
-void word_FUENF2() {
-  toStripe(X8Y5);
-  toStripe(X9Y5);
-  toStripe(X10Y5);
-  toStripe(X11Y5);
-}
-
-void word_SECHS() {
-  toStrip(X1Y8);
-  toStrip(X2Y8);
-  toStrip(X3Y8);
-  toStrip(X4Y8);
-  toStrip(X5Y8);
-}
-
-void word_SIEBEN() {
-  toStrip(X1Y9);
-  toStrip(X2Y9);
-  toStrip(X3Y9);
-  toStrip(X4Y9);
-  toStrip(X5Y9);
-  toStrip(X6Y9);
-}
-
-void word_ACHT() {
-  toStrip(X8Y8);
-  toStrip(X9Y8);
-  toStrip(X10Y8);
-  toStrip(X11Y8);
-}
-
-void word_NEUN() {
-  toStrip(X4Y10);
-  toStrip(X5Y10);
-  toStrip(X6Y10);
-  toStrip(X7Y10);
-}
-
-void word_ZEHN1() {
-  toStrip(X1Y2);
-  toStrip(X2Y2);
-  toStrip(X3Y2);
-  toStrip(X4Y2);
-}
-
-void word_ZEHN2() {
-  toStrip(X1Y10);
-  toStrip(X2Y10);
-  toStrip(X3Y10);
-  toStrip(X4Y10);
-}
-
-void word_ELF() {
-  toStrip(X6Y5);
-  toStrip(X7Y5);
-  toStrip(X8Y5);
-}
-
-void word_ZWOELF() {
-  toStrip(X7Y9);
-  toStrip(X8Y9);
-  toStrip(X9Y9);
-  toStrip(X10Y9);
-  toStrip(X11Y9);
-}
-
-void word_ZWANZIG() {
-  toStrip(X5Y2);
-  toStrip(X6Y2);
-  toStrip(X7Y2);
-  toStrip(X8Y2);
-  toStrip(X9Y2);
-  toStrip(X10Y2);
-  toStrip(X11Y2);
-}
+const int word_EIN[]     {X2Y6, X3Y6, X4Y6};
+const int word_EINS[]    {X2Y6, X3Y6, X4Y6, X5Y6};
+const int word_ZWEI[]    {X8Y8, X9Y8, X10Y8, X11Y8};
+const int word_DREI[]    {X1Y7, X2Y7, X3Y7, X4Y7};
+const int word_VIER[]    {X8Y7, X9Y7, X10Y7, X11Y7};
+const int word_FUENF1[]  {X8Y1, X9Y1, X10Y1, X11Y1};
+const int word_FUENF2[]  {X8Y5, X9Y5, X10Y5, X11Y5};
+const int word_SECHS[]   {X1Y8, X2Y8, X3Y8, X4Y8, X5Y8};
+const int word_SIEBEN[]  {X1Y9, X2Y9, X3Y9, X4Y9, X5Y9, X6Y9};
+const int word_ACHT[]    {X8Y8, X9Y8, X10Y8, X11Y8};
+const int word_NEUN[]    {X4Y10, X5Y10, X6Y10, X7Y10};
+const int word_ZEHN1[]   {X1Y2, X2Y2, X3Y2, X4Y2};
+const int word_ZEHN2[]   {X1Y10, X2Y10, X3Y10, X4Y10};
+const int word_ELF[]     {X6Y5, X7Y5, X8Y5};
+const int word_ZWOELF[]  {X7Y9, X8Y9, X9Y9, X10Y9, X11Y9};
+const int word_ZWANZIG[] {X5Y2, X6Y2, X7Y2, X8Y2, X9Y2, X10Y2, X11Y2};
