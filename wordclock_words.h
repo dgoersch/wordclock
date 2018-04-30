760ab0ee94fcd5c3544274748a120bbb58e08635
per1234 <accounts@perglass.com>
2018-04-30 16:18:45 -0700

    Correct syntax of word initializations
    Prior to this change, compilation would fail:
    
    arduino_build_605313\sketch\wordclock_words.c:44:26: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
    
     const int min_ONE[]      {MIN1};
    
                              ^
    
    etc...

--- wordclock_words.h
+++ wordclock_words.h
@@ -41,36 +41,36 @@ Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
  
 #include "wordclock_layout1.h"
 
-const int min_ONE[]      {MIN1};
-const int min_TWO[]      {MIN2};
-const int min_THREE[]    {MIN3};
-const int min_FOUR[]     {MIN4};
+const int min_ONE[] =      {MIN1};
+const int min_TWO[] =      {MIN2};
+const int min_THREE[] =    {MIN3};
+const int min_FOUR[] =     {MIN4};
 
-const int word_KEIN[]    {X1Y6, X2Y6, X3Y6, X4Y6};
-const int word_FUNK[]    {X4Y4, X5Y4, X5Y4, X7Y4};
+const int word_KEIN[] =    {X1Y6, X2Y6, X3Y6, X4Y6};
+const int word_FUNK[] =    {X4Y4, X5Y4, X5Y4, X7Y4};
 
-const int word_ES[]      {X1Y1, X2Y1};
-const int word_IST[]     {X4Y1, X5Y1, X6Y1};
-const int word_UHR[]     {X9Y10, X10Y10, X11Y10};
+const int word_ES[] =      {X1Y1, X2Y1};
+const int word_IST[] =     {X4Y1, X5Y1, X6Y1};
+const int word_UHR[] =     {X9Y10, X10Y10, X11Y10};
 
-const int word_VOR[]     {X1Y4, X2Y4, X3Y4};
-const int word_NACH[]    {X8Y4, X9Y4, X10Y4, X11Y4};
-const int word_VIERTEL[] {X5Y3, X6Y3, X7Y3, X8Y3, X9Y3, X10Y3, X11Y3};
-const int word_HALB[]    {X1Y5, X2Y5, X3Y5, X4Y5};
+const int word_VOR[] =     {X1Y4, X2Y4, X3Y4};
+const int word_NACH[] =    {X8Y4, X9Y4, X10Y4, X11Y4};
+const int word_VIERTEL[] = {X5Y3, X6Y3, X7Y3, X8Y3, X9Y3, X10Y3, X11Y3};
+const int word_HALB[] =    {X1Y5, X2Y5, X3Y5, X4Y5};
 
-const int word_EIN[]     {X2Y6, X3Y6, X4Y6};
-const int word_EINS[]    {X2Y6, X3Y6, X4Y6, X5Y6};
-const int word_ZWEI[]    {X8Y8, X9Y8, X10Y8, X11Y8};
-const int word_DREI[]    {X1Y7, X2Y7, X3Y7, X4Y7};
-const int word_VIER[]    {X8Y7, X9Y7, X10Y7, X11Y7};
-const int word_FUENF1[]  {X8Y1, X9Y1, X10Y1, X11Y1};
-const int word_FUENF2[]  {X8Y5, X9Y5, X10Y5, X11Y5};
-const int word_SECHS[]   {X1Y8, X2Y8, X3Y8, X4Y8, X5Y8};
-const int word_SIEBEN[]  {X1Y9, X2Y9, X3Y9, X4Y9, X5Y9, X6Y9};
-const int word_ACHT[]    {X8Y8, X9Y8, X10Y8, X11Y8};
-const int word_NEUN[]    {X4Y10, X5Y10, X6Y10, X7Y10};
-const int word_ZEHN1[]   {X1Y2, X2Y2, X3Y2, X4Y2};
-const int word_ZEHN2[]   {X1Y10, X2Y10, X3Y10, X4Y10};
-const int word_ELF[]     {X6Y5, X7Y5, X8Y5};
-const int word_ZWOELF[]  {X7Y9, X8Y9, X9Y9, X10Y9, X11Y9};
-const int word_ZWANZIG[] {X5Y2, X6Y2, X7Y2, X8Y2, X9Y2, X10Y2, X11Y2};
+const int word_EIN[] =     {X2Y6, X3Y6, X4Y6};
+const int word_EINS[] =    {X2Y6, X3Y6, X4Y6, X5Y6};
+const int word_ZWEI[] =    {X8Y8, X9Y8, X10Y8, X11Y8};
+const int word_DREI[] =    {X1Y7, X2Y7, X3Y7, X4Y7};
+const int word_VIER[] =    {X8Y7, X9Y7, X10Y7, X11Y7};
+const int word_FUENF1[] =  {X8Y1, X9Y1, X10Y1, X11Y1};
+const int word_FUENF2[] =  {X8Y5, X9Y5, X10Y5, X11Y5};
+const int word_SECHS[] =   {X1Y8, X2Y8, X3Y8, X4Y8, X5Y8};
+const int word_SIEBEN[] =  {X1Y9, X2Y9, X3Y9, X4Y9, X5Y9, X6Y9};
+const int word_ACHT[] =    {X8Y8, X9Y8, X10Y8, X11Y8};
+const int word_NEUN[] =    {X4Y10, X5Y10, X6Y10, X7Y10};
+const int word_ZEHN1[] =   {X1Y2, X2Y2, X3Y2, X4Y2};
+const int word_ZEHN2[] =   {X1Y10, X2Y10, X3Y10, X4Y10};
+const int word_ELF[] =     {X6Y5, X7Y5, X8Y5};
+const int word_ZWOELF[] =  {X7Y9, X8Y9, X9Y9, X10Y9, X11Y9};
+const int word_ZWANZIG[] = {X5Y2, X6Y2, X7Y2, X8Y2, X9Y2, X10Y2, X11Y2};
